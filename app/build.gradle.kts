df9161f9c8a83d306f07967601b2f49ef2c46577
agungatd <agungatidhira@gmail.com>
2024-04-23 11:03:43 +0700

    add postgresql driver dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    // Database drivers
+    implementation("org.postgresql:postgresql:42.7.3")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
